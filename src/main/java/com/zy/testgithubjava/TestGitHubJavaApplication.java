a45267e2a3de45736aab1a462b1086168df44ac3
chensong <chens@zhongyitech.com>
2023-07-25 09:33:24 +0800

    初始化项目

---
+++ src/main/java/com/zy/testgithubjava/TestGitHubJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zy.testgithubjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitHubJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitHubJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zy/testgithubjava/TestGitHubJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.testgithubjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitHubJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
